3b422890c5c5ebadacb9735d9f0b0df60222610b
mrx <sajjadzibaee102@gmail.com>
2022-03-23 20:01:35 +0430

    first

---
+++ src/views/todo.jsx
@@ -0,0 +1,19 @@
+import { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+class Todo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Todo</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Todo
